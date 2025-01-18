6d422830d46148115a2c71bd5f5b7343ffb3343a
faltrum <70612135+faltrum@users.noreply.github.com>
2025-01-18 21:27:03 +0000

    Invite cpp

--- src/conexion/Client.cpp
+++ src/conexion/Client.cpp
@@ -40,10 +40,10 @@ Client &Client::operator=(Client const &src)
 ###############################################################################
 */
 void Client::SetFd(int fd){this->_fd = fd;}
-void Client::setIpAdd(std::string ipadd){this->_IPadd = ipadd;}
+void Client::SetIpAdd(std::string ipadd){this->_IPadd = ipadd;}
 void Client::SetNickname(std::string& nickName){this->_nickname = nickName;}
 void Client::SetUsername(std::string& username){this->_username = username;}
-void Client::setBuffer(std::string recived){this->_buffer += recived;}
+void Client::SetBuffer(std::string recived){this->_buffer += recived;}
 
 /*
 ###############################################################################
@@ -51,10 +51,10 @@ void Client::setBuffer(std::string recived){this->_buffer += recived;}
 ###############################################################################
 */
 int Client::GetFd(){return this->_fd;}
-std::string Client::getIpAdd(){return _IPadd;}
+std::string Client::GetIpAdd(){return _IPadd;}
 std::string Client::GetNickName(){return this->_nickname;}
 std::string Client::GetUserName(){return this->_username;}
-std::string Client::getBuffer(){return this->_buffer;}
+std::string Client::GetBuffer(){return this->_buffer;}
 
 /*
 ###############################################################################
