4effb236a0157127ee159ecc4d66c596504919fc
faltrum <70612135+faltrum@users.noreply.github.com>
2025-01-27 16:10:03 +0000

    debugging errors doing make

--- .dps/Channels.d
+++
@@ -1,5 +0,0 @@
-.obj/conexion/Channels.o: src/conexion/Channels.cpp \
- includes/conexion/Channel.hpp includes/conexion/Client.hpp \
- includes/conexion/Server.hpp includes/conexion/Channel.hpp \
- includes/comunication/Errors.hpp includes/comunication/Endline.hpp \
- includes/comunication/Information.hpp includes/functions/AllFunctions.h
--- .dps/ValidationServer.d
+++
@@ -1,6 +0,0 @@
-.obj/conexion/serverFunctions/ValidationServer.o: \
- src/conexion/serverFunctions/ValidationServer.cpp \
- includes/conexion/Server.hpp includes/conexion/Client.hpp \
- includes/conexion/Server.hpp includes/conexion/Channel.hpp \
- includes/comunication/Errors.hpp includes/comunication/Endline.hpp \
- includes/comunication/Information.hpp includes/functions/AllFunctions.h
